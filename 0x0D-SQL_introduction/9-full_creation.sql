10ddf1602622a52ab1bd026f24dc680b77dc93f4
AbYahaya <yabdulrauf911@gmail.com>
2024-02-15 04:59:18 -0800

    Task 10

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- Creates another table
+-- DDL query to create a new table
+CREATE TABLE  IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+-- TO add first row
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+-- DML query to insert Second row
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+-- DML query to insert third row
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+-- DML quey to add fourth row
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
