a69b21b240d983069c7b14fa61d5c250f5cf151c
AbYahaya <yabdulrauf911@gmail.com>
2024-03-17 05:41:20 -0700

    Task 2 and 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Created by Ab Yahaya
+Created by Ab yahaya
 """
 import MySQLdb
 import sys
@@ -8,38 +8,20 @@ import sys
 
 if __name__ == '__main__':
     args = sys.argv
-    if len(args) != 5:
-        print("Usage: {} username password database_name state_name"
-              .format(args[0]))
+    if len(args) < 5:
+        print("Usage: {} username password database_name".format(args[0]))
         exit(1)
     username = args[1]
     password = args[2]
-    database = args[3]
+    data = args[3]
     state_name = args[4]
-
-    try:
-        # Connect to MySQL database
-        db = MySQLdb.connect(host='localhost', user=username
-                             passwd=password, db=database, port=3306)
-        cur = db.cursor()
-
-        # Execute SQL query
-        num_rows = cur.execute("SELECT * FROM states WHERE states.name\
-                               LIKE BINARY %s ORDER BY states.id;"
-                               (state_name,))
-        rows = cur.fetchall()
-
-        # Print query results
-        for row in rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("MySQL Error: {}".format(e))
-        exit(1)
-
-    finally:
-        # Close cursor and database connection
-        if 'cur' in locals():
-            cur.close()
-        if 'db' in locals():
-            db.close()
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=data, port=3306)
+    cur = db.cursor()
+    num_rows = cur.execute("SELECT * FROM states WHERE states.name LIKE BINARY\
+                           %s ORDER BY states.id;", (state_name,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
