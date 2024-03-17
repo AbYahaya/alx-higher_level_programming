d8d78930b1476f9aa5aeec500fff51d38bd24b69
AbYahaya <yabdulrauf911@gmail.com>
2024-03-17 05:01:37 -0700

    Some latest

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,19 +9,37 @@ import sys
 if __name__ == '__main__':
     args = sys.argv
     if len(args) != 5:
-        print("Usage: {} username password database_name".format(args[0]))
+        print("Usage: {} username password database_name state_name"
+              .format(args[0]))
         exit(1)
     username = args[1]
     password = args[2]
-    data = args[3]
+    database = args[3]
     state_name = args[4]
-    db = MySQLdb.connect(host='localhost', user=username,
-                         passwd=password, db=data, port=3306)
-    cur = db.cursor()
-    num_rows = cur.execute("SELECT * FROM states WHERE states.name LIKE BINARY\
-                           %s ORDER BY states.id;", (state_name,))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+
+    try:
+        # Connect to MySQL database
+        db = MySQLdb.connect(host='localhost', user=username
+                             passwd=password, db=database, port=3306)
+        cur = db.cursor()
+
+        # Execute SQL query
+        num_rows = cur.execute("SELECT * FROM states WHERE states.name\
+                               LIKE BINARY %s ORDER BY states.id;"
+                               (state_name,))
+        rows = cur.fetchall()
+
+        # Print query results
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
+        exit(1)
+
+    finally:
+        # Close cursor and database connection
+        if 'cur' in locals():
+            cur.close()
+        if 'db' in locals():
+            db.close()
