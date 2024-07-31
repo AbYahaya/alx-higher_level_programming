96b89edbb7972e198c0034d0ee1f964458bc0294
AbYahaya <yabdulrauf911@gmail.com>
2024-07-31 12:01:15 -0700

    Last task

--- 101-starwars_characters.js
+++ 101-starwars_characters.js
@@ -1,4 +1,3 @@
-#!/usr/bin/node
 const request = require('request');
 
 if (process.argv.length !== 3) {
@@ -13,24 +12,33 @@ request(url, { json: true }, (err, res, body) => {
     if (err) {
         return console.log(err);
     }
-
+    
     if (res.statusCode !== 200) {
         return console.log(`Error: Unable to fetch data for movie ID ${movieId}`);
     }
 
     const characters = body.characters;
-
-    characters.forEach(characterUrl => {
-        request(characterUrl, { json: true }, (err, res, body) => {
+    
+    // Function to fetch and print character names sequentially
+    const fetchCharacterNames = (index) => {
+        if (index >= characters.length) {
+            return;
+        }
+        
+        request(characters[index], { json: true }, (err, res, body) => {
             if (err) {
                 return console.log(err);
             }
-
+            
             if (res.statusCode === 200) {
                 console.log(body.name);
+                fetchCharacterNames(index + 1);
             } else {
-                console.log(`Error: Unable to fetch character data from ${characterUrl}`);
+                console.log(`Error: Unable to fetch character data from ${characters[index]}`);
             }
         });
-    });
+    };
+    
+    // Start fetching character names
+    fetchCharacterNames(0);
 });
